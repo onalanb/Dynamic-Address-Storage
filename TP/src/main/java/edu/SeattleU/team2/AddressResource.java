3cb2e56b23fdb0d60a22eac1b242572088ec573e
Baran Onalan <55814942+onalanb@users.noreply.github.com>
2023-02-16 21:25:20 -0800

    Update AddressResource.java

--- TP/src/main/java/edu/SeattleU/team2/AddressResource.java
+++ TP/src/main/java/edu/SeattleU/team2/AddressResource.java
@@ -9,6 +9,7 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -20,26 +21,80 @@ public class AddressResource {
     private static int ID = 1;
     private static HashMap<Integer, Address> addressTable = new HashMap<Integer, Address>();
 
+    private Connection connection = null;
+
+    // Making sure the JDBC driver is loaded.
+    private AddressResource() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/TeamProject?useSSL=false", "root", "password");
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
     // CREATE
     // curl -X POST http://localhost:8080/TP-1.0-SNAPSHOT/api/address -H "Content-Type: application/json" -d "{\"country\":\"USA\",\"recipient\":\"Michelle Malone\",\"streetAddress\":\"123 Troll Ave NE\",\"postalCode\":\"98008\",\"city_town_locality\":\"Seattle\",\"state\":\"WA\"}"
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response createNewAddress(Address address) {
-        address.setID(ID);
-        addressTable.put(ID, address);
-        // System.out.println(address.getRecipient());
-        ID++;
-        return Response.ok(address).build();
+        try {
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO Address (country, recipient, streetAddress, postalCode, city_town_locality, state) VALUES (?, ?, ?, ?, ?, ?)",
+                    PreparedStatement.RETURN_GENERATED_KEYS);
+            statement.setString(1, address.getCountry());
+            statement.setString(2, address.getRecipient());
+            statement.setString(3, address.getStreetAddress());
+            statement.setString(4, address.getPostalCode());
+            statement.setString(5, address.getCity_town_locality());
+            statement.setString(6, address.getState());
+            int rowsInserted = statement.executeUpdate();
+            if (rowsInserted > 0) {
+                ResultSet generatedKeys = statement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    int id = generatedKeys.getInt(1);
+                    address.setID(id);
+                    return Response.ok(address).build();
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Response.serverError().build();
     }
 
-    // READ
+    // READ - DONE
     // curl  -X GET http://localhost:8080/TP-1.0-SNAPSHOT/api/address/1 -H "Accept: application/json"
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/{intID}")
     public Response getAddress(@PathParam("intID") Integer id) {
-        Address address = addressTable.get(id);
+        Address address = null;
+
+        try {
+            PreparedStatement pstmt = connection.prepareStatement(
+                    "SELECT ID, country, recipient, streetAddress, postalCode, city_town_locality, state FROM Address WHERE ID = ?");
+            pstmt.setInt(1, id);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                String country = rs.getString("country");
+                String recipient = rs.getString("recipient");
+                String streetAddress = rs.getString("streetAddress");
+                String postalCode = rs.getString("postalCode");
+                String city_town_locality = rs.getString("city_town_locality");
+                String state = rs.getString("state");
+                address = new Address(id, country, recipient, streetAddress, postalCode, city_town_locality, state);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         // System.out.println(address.getRecipient());
         return Response.ok(address).build();
     }
@@ -50,14 +105,25 @@ public class AddressResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("/{intID}")
     public Response updateAddress(@PathParam("intID") Integer id, Address address) {
-        Address oldAddress = addressTable.get(id);
-        if (oldAddress == null) {
-            return Response.status(404).build();
+        try {
+            PreparedStatement pstmt = connection.prepareStatement(
+                    "UPDATE Address SET country = ?, recipient = ?, streetAddress = ?, postalCode = ?, city_town_locality = ?, state = ? WHERE ID = ?");
+            pstmt.setString(1, address.getCountry());
+            pstmt.setString(2, address.getRecipient());
+            pstmt.setString(3, address.getStreetAddress());
+            pstmt.setString(4, address.getPostalCode());
+            pstmt.setString(5, address.getCity_town_locality());
+            pstmt.setString(6, address.getState());
+            pstmt.setInt(7, id);
+            int rowsUpdated = pstmt.executeUpdate();
+            if (rowsUpdated > 0) {
+                address.setID(id); // check if this fix worked
+                return Response.ok(address).build();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        address.setID(id);
-        addressTable.put(id, address);
-        // System.out.println(address.getRecipient());
-        return Response.ok(address).build();
+        return Response.status(404).entity("Client entered wrong value.").type("text/plain").build();
     }
 
     // DELETE
@@ -65,13 +131,18 @@ public class AddressResource {
     @DELETE
     @Path("/{intID}")
     public Response deleteAddress(@PathParam("intID") Integer id) {
-        Address address = addressTable.get(id);
-        if (address == null) {
-            return Response.status(404).build();
+        try {
+            PreparedStatement pstmt = connection.prepareStatement("DELETE FROM Address WHERE ID = ?");
+            pstmt.setInt(1, id);
+            int rowsDeleted = pstmt.executeUpdate();
+            if (rowsDeleted == 0) {
+                return Response.status(404).build();
+            }
+            return Response.ok().build();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return Response.status(500).build();
         }
-        addressTable.remove(id);
-        // System.out.println(address.getRecipient());
-        return Response.ok().build();
     }
 
     // SEARCH
@@ -79,19 +150,31 @@ public class AddressResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     // TO-DO
-    // Add more query parameters for more search filters.
+    // Add more query parameters for more search filters. Also edit the Prepared Statement.
     public Response searchAddress(@QueryParam("country") String country,
                                   @QueryParam("recipient") String recipient) {
         ArrayList<Address> result = new ArrayList<Address>();
-        for (Address address : addressTable.values()) {
-            boolean countryMatch = country == null || country.isEmpty() || country.equals(address.getCountry());
-            boolean recipientMatch = recipient == null || recipient.isEmpty() || recipient.equals(address.getRecipient());
-            if (countryMatch && recipientMatch) {
-                result.add(address);
+        try {
+            PreparedStatement pstmt = connection.prepareStatement(
+                    "SELECT ID, country, recipient, streetAddress, postalCode, city_town_locality, state FROM Address WHERE country LIKE ? AND recipient LIKE ?");
+            pstmt.setString(1, "%" + (country == null ? "" : country) + "%");
+            pstmt.setString(2, "%" + (recipient == null ? "" : recipient) + "%");
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("ID");
+                String retrievedCountry = rs.getString("country");
+                String retrievedRecipient = rs.getString("recipient");
+                String retrievedStreetAddress = rs.getString("streetAddress");
+                String retrievedPostalCode = rs.getString("postalCode");
+                String retrievedCityTownLocality = rs.getString("city_town_locality");
+                String retrievedState = rs.getString("state");
+                result.add(new Address(id, retrievedCountry, retrievedRecipient, retrievedStreetAddress, retrievedPostalCode, retrievedCityTownLocality, retrievedState));
             }
+            return Response.ok(result).build();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return Response.status(500).build();
         }
-
-        return Response.ok(result).build();
     }
 
 }
