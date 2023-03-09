d0b835ba3d05e49f82b49adb19313ce363685fbd
troy8chen <troy8chen@gmail.com>
2023-03-08 21:36:24 -0800

    Update ModTable and TeamProject.nwb

---
+++ TP/src/main/java/edu/SeattleU/team2/ModTable.java
@@ -0,0 +1,117 @@
+package edu.SeattleU.team2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
+
+public class ModTable {
+    public static void main(String[] args) {
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/TeamProject?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+        String userName = "root";
+        String password = "password";
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, userName, password);
+            System.out.println("Connected to database!");
+            stmt = conn.createStatement();
+
+            // Drop the addresses table if it exists
+            stmt.executeUpdate("DROP TABLE IF EXISTS `addresses`");
+
+            // Create the addresses table
+            String createTableSQL = "CREATE TABLE `addresses` (" +
+                    "`id` int NOT NULL AUTO_INCREMENT," +
+                    "`country` varchar(255)," +
+                    "`state_province` varchar(255)," +
+                    "`recipient` varchar(255)," +
+                    "`street_number` varchar(255)," +
+                    "`street_address` varchar(255)," +
+                    "`postal_code` varchar(255)," +
+                    "`city_town_locality` varchar(255)," +
+                    "`address_format` varchar(255)," +
+                    "PRIMARY KEY (`id`)" +
+                    ")";
+
+            stmt.executeUpdate(createTableSQL);
+
+            // Add the full_address column to the addresses table
+            String addColumnSQL = "ALTER TABLE `addresses` ADD COLUMN `full_address` VARCHAR(255) DEFAULT NULL AFTER `address_format`";
+            stmt.executeUpdate(addColumnSQL);
+
+            // Set all values of full_address to NULL again
+            String setNullSQL = "UPDATE `addresses` SET `full_address`=NULL";
+            stmt.executeUpdate(setNullSQL);
+
+            // Execute the SQL script file
+            String sqlScriptFilePath = "/Users/troy8chen/Desktop/2023 Winter/CPSC5200/Dynamic-Address-Storage/TP/src/main/java/edu/SeattleU/team2/addressesWITH_DATA.sql";
+            BufferedReader br = new BufferedReader(new FileReader(sqlScriptFilePath));
+            String line;
+            StringBuilder sb = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+                System.out.println(line);
+            }
+            br.close();
+            String[] sqlStatements = sb.toString().split(";");
+            for (String sqlStatement : sqlStatements) {
+                System.out.println(sqlStatement);
+                if (sqlStatement.trim().toLowerCase().startsWith("select")) {
+                    rs = stmt.executeQuery(sqlStatement);
+                    ResultSetMetaData metaData = rs.getMetaData();
+                    int columnCount = metaData.getColumnCount();
+                    while (rs.next()) {
+                        for (int i = 1; i <= columnCount; i++) {
+                            String columnName = metaData.getColumnName(i);
+                            Object value = rs.getObject(i);
+                            System.out.println(columnName + ": " + value);
+                        }
+                        System.out.println();
+                    }
+                } else {
+                    stmt.executeUpdate(sqlStatement);
+                }
+            }
+            System.out.println("SQL script file executed successfully!");
+            // Print all rows in the addresses table
+            rs = stmt.executeQuery("SELECT * FROM `addresses`");
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            while (rs.next()) {
+                for (int i = 1; i <= columnCount; i++) {
+                    String columnName = metaData.getColumnName(i);
+                    Object value = rs.getObject(i);
+                    System.out.println(columnName + ": " + value);
+                }
+                System.out.println();
+            }
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stmt != null) {
+                    stmt.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
